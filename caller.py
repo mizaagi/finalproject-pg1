88c28927e775babf0fcf051adf0f5f9779d947e6
Marlon Kohn <126592834+mizaagi@users.noreply.github.com>
2024-01-11 13:30:46 -0600

    Deleted unnecessary time.sleep() (fixed), added test print function (commented out)

--- caller.py
+++ caller.py
@@ -2,11 +2,11 @@ import pygame
 import time
 
 def Call(square, lvl_pause):
+  #print("c.Call | Test Print")
   _tick = 0
   square.image = pygame.image.load('call_square.png')
   square.tapped = False
   start_time = time.time()
-  time.sleep(lvl_pause)
   while square.tapped == False:
     _tick += 1
   end_time = time.time()
