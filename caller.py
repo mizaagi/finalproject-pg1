b36f93d6881575af5c93cfdf0f412a96edb56867
Marlon Kohn <126592834+mizaagi@users.noreply.github.com>
2024-01-16 13:17:20 -0600

    Fixed issues with arguments, increases score when pressed in time (v1)

--- caller.py
+++ caller.py
@@ -2,7 +2,7 @@ import pygame
 import time
 import main
 
-def Call(square, lvl_pause):
+def Call(square, lvl_pause, ins):
   #print("c.Call | Test Print")
   _tick = 0
   square.image = pygame.image.load('call_square.png')
@@ -10,12 +10,13 @@ def Call(square, lvl_pause):
   start_time = time.time()
   while square.tapped == False:
     _tick += 1
-    main.ins.yourTick(str(_tick))
+    ins.yourTick(str(_tick))
   # Work on possibly checking if the player has pressed the square within [lvl_pause] seconds (right away).
   end_time = time.time()
   if round((end_time - start_time), 2) > lvl_pause:
     square.gotit = False
-    main.ins.accList.append(0)
+    ins.accList.append(0)
   else:
     square.gotit = True
-    main.ins.accList.append(100)
+    ins.accList.append(100)
+    ins.score += 1
